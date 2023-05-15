34e8eab8a0d0db3ef977bedf889d839377e198ea
Mariusz Bojkowski <Mariusz_Bojkowski@bojkowski.it>
2023-05-15 17:10:37 +0200

    Use x64 platform for builds

--- build/Build.cs
+++ build/Build.cs
@@ -4,5 +4,7 @@ using Nuke.Useful.Builds;
 [UnsetVisualStudioEnvironmentVariables]
 class Build : AzureDevOpsLibraryBuild
 {
+    public Build() => Platform = "x64";
+
     public static int Main() => Execute<Build>(x => x.RunAllSteps);
 }
