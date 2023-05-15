5dca139e0b1d83601308bee261966c1354e6df6a
Mariusz Bojkowski <Mariusz_Bojkowski@bojkowski.it>
2023-05-15 15:09:31 +0200

    Update build project to .NET 6

--- build/Build.cs
+++ build/Build.cs
@@ -1,9 +1,8 @@
 using Nuke.Common.Execution;
 using Nuke.Useful.Builds;
 
-[CheckBuildProjectConfigurations]
 [UnsetVisualStudioEnvironmentVariables]
 class Build : AzureDevOpsLibraryBuild
 {
-    public static int Main () => Execute<Build>(x => x.BuildAzureDevOpsLibrary);
+    public static int Main() => Execute<Build>(x => x.RunAllSteps);
 }
